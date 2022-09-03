b782f4b2b99425803307d1cae1bc61dee6978466
Kai <7115703+kai13xd@users.noreply.github.com>
2022-09-02 16:58:58 -0800

    Revert "read in index in separate line"
    This reverts commit 2ae8fcd062f6982467b01c27a18583f49dad6196.

--- SuperBMDLib/source/BMD/MAT3.cs
+++ SuperBMDLib/source/BMD/MAT3.cs
@@ -312,8 +312,7 @@ namespace SuperBMDLib.BMD
                 Console.WriteLine("Warning: Material {0} referenced an out of range IndirectTexBlock index", mat.Name);
             }
             mat.ZCompLoc = m_zCompLocBlock[reader.ReadByte()];
-            int zmode_index = reader.ReadByte();
-            mat.ZMode = m_zModeBlock[zmode_index];
+            mat.ZMode = m_zModeBlock[reader.ReadByte()];
 
             if (m_ditherBlock == null)
                 reader.SkipByte();
